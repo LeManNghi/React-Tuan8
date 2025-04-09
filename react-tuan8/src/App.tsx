fcc0f6de6f066088d0e434c1b2756e90c18c7371
H82M19\Student <Student>
2025-04-09 19:56:48 +0700

    Create Project + Layout

---
+++ react-tuan8/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='container'>
+        <div className='header'>Header</div>
+        <div className='content'>Content</div>
+        <div className='footer'>Footer</div>
+      </div>
+    </>
+  )
+}
+
+export default App
